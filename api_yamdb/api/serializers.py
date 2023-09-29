f0e50d9c38e196deef9cbc6c6b8d3c64e704b242
Иван Хомюк <mr.homyuk@mail.ru>
2023-09-29 11:32:14 +0900

    in process.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -12,16 +12,23 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, value):
-        if MyUser.objects.filter(username=value.get('username')):
-            raise serializers.ValidationError(
-                'Такой пользователь уже существует'
-            )
-        if MyUser.objects.filter(email=value.get('email')):
-            raise serializers.ValidationError(
-                'Пользователь с такой почтой уже существует'
-            )
+        # if MyUser.objects.filter(username=value.get('username')):
+        #     raise serializers.ValidationError(
+        #         'Такой пользователь уже существует'
+        #     )
+        # if MyUser.objects.filter(email=value.get('email')):
+        #     raise serializers.ValidationError(
+        #         'Пользователь с такой почтой уже существует'
+        #     )
         if value.get('username') == 'me':
             raise serializers.ValidationError(
                 'Запрещено и спользовать имя me'
             )
         return value
+
+
+class CreateUserSerialiser(UserSerializer):
+    """Сериалайзер для создания объекта модели User."""     # Хрень
+    class Meta:
+        model = MyUser
+        fields = ('email', 'username')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,13 +5,25 @@ from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 
 from api.permissions import IsOwnerOrReadOnly, ReadOnly
-from api.serializers import UserSerializer
+from api.serializers import CreateUserSerialiser, UserSerializer
 from users.models import MyUser
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
     queryset = MyUser.objects.all()
     lookup_field = 'username'
+    http_method_names = ('get', 'post', 'patch', 'delete')
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username',)
+
+    def get_object(self):
+        username = self.kwargs.get('username')
+        if username == 'me':
+            return get_object_or_404(MyUser, username=self.request.user)
+        return get_object_or_404(MyUser, username=username)
+
+    def get_serializer_class(self):
+        username = self.kwargs.get('username')
+        if username == 'me':
+            return CreateUserSerialiser
+        return UserSerializer
